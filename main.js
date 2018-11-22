4bed2ef249bab12fddfa0c770b474edd99798cdf
childrem <38900005+childrem@users.noreply.github.com>
2018-11-22 12:22:02 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ var express = require('express');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+var mysql = require('./dbcon.js');
+
+app.use(express.static('public'));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
