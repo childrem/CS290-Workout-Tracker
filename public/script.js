fd38d5953c93dda204d264cd4c9989ea591c1928
childrem <38900005+childrem@users.noreply.github.com>
2018-11-22 14:17:15 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -14,6 +14,17 @@ function setupButton(){
 
 document.getElementById('addExerciseButton').addEventListener("click", function(event){
   console.log("Event Listener Simple Method Worked!");
+  var req = new XMLHttpRequest();
+  req.open("GET", "http://flip2.engr.oregonstate.edu:5840/", true);
+  req.addEventListener("load", function(){
+    if (req.status >= 200 && req.status < 400) {
+      console.log("GET Request Worked");
+    }
+    
+    else {
+      console.log("Get Request Failed");
+    }
+  });
   
   event.preventDefault();
 });
