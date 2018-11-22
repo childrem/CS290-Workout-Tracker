ab42c854c532acd5889da468a3bd377a86faed63
childrem <38900005+childrem@users.noreply.github.com>
2018-11-22 11:52:27 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,1 +1,11 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host : 'classmysql.engr.oregonstate.edu',
+  user : 'cs290_yyyy',
+  password : 'xxxx',
+  database : 'cs290_yyyy'
+  dateStrings : 'date'
+});
 
+module.exports.pool = pool;
