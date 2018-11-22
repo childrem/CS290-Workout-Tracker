3f63a173ab4cf34b23e0b00416e7841edab8eb99
childrem <38900005+childrem@users.noreply.github.com>
2018-11-22 13:19:27 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,8 @@
 function submitButtonTest(){
   console.log("The button submitted a GET Request");
 };
+
+
+document.getElementById('addExerciseButton').addEventListener("click", function(){
+  
+});
