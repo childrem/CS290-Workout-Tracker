740f61a8e4c3313e18cce4f67300405d86a5d156
childrem <38900005+childrem@users.noreply.github.com>
2018-11-23 11:09:11 -0800

    Update main.js

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ app.post('/', function(req,res,next){
   console.log("POST Request Received By Server!");
   
   if(req.body.addExerciseButton){   // We came in from the add exercise form so need to add a row to the database table
-    mysql.pool.query('INSERT INTO workouts (name, reps, weight, date, lbs) VALUES (?,?,?,?,?), [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.lbs], function(err, result){
+    mysql.pool.query('INSERT INTO workouts (name, reps, weight, date, lbs) VALUES (?,?,?,?,?)', [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.lbs], function(err, result){
       if(err){
         next(err);
         return;
