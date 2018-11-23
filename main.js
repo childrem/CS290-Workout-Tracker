b1e3cb1927583507ac6026133d7d4ae02870c550
childrem <38900005+childrem@users.noreply.github.com>
2018-11-23 12:44:07 -0800

    Update main.js

--- main.js
+++ main.js
@@ -73,8 +73,8 @@ app.post('/', function(req,res,next){
                 else {
                   res.send(JSON.stringify(rows));
                   
-                  context.results = JSON.stringify(rows);
-                  res.render('home', context);
+                  //context.results = JSON.stringify(rows);
+                  //res.render('home', context);
                 }
             });
         }
