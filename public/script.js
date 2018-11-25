f131d7561346fcc9829b544d6ce54609b45ac8e1
childrem <38900005+childrem@users.noreply.github.com>
2018-11-24 16:10:45 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -213,7 +213,7 @@ function bindDeleteButtons(){
 
   for (var index = 0; index < deleteButtonList.length; index++){
       deleteButtonList[index].addEventListener("click", function(event){
-      console.log("Event worked for delete button!");
+      console.log("Event worked for delete button with id# " + hiddenIdList[index].id);
       event.preventDefault();
     });
   }
